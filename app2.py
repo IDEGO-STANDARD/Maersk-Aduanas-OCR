953e9f89c8abd4c2d61b4b165ddf45bb65d7d693
Piero Angelo Morales Alcalde <38793411+Piero16301@users.noreply.github.com>
2023-10-24 22:30:16 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -226,4 +226,4 @@ def recognize_pdf():
     return {'type': result_label, 'resultados': resultados_lista}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
