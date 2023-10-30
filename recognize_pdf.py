a8de609005cbee01f86bda2ca9aa84ce252903e2
NicolasFG <nicolas.figueroa@utec.edu.pe>
2023-10-30 05:08:29 -0500

    .

--- recognize_pdf.py
+++ recognize_pdf.py
@@ -307,5 +307,5 @@ def recognize_pdf():
 def hello():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port="8400",debug=True)
